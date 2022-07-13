8e257816eaf0b8d59f7983671378fd86f9cd29b1
gabriel-olv <br.gabrieldeoliveira@gmail.com>
2022-07-13 15:44:19 -0300

    Date select

--- backend/src/main/java/br/com/gabrieldeoliveira/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/br/com/gabrieldeoliveira/dsmeta/controllers/SaleController.java
@@ -1,10 +1,11 @@
 package br.com.gabrieldeoliveira.dsmeta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gabrieldeoliveira.dsmeta.entities.Sale;
@@ -18,7 +19,9 @@ public class SaleController {
 	private SaleService saleService;
 	
 	@GetMapping
-	public List<Sale> getSales() {
-		return saleService.findSales();
+	public Page<Sale> getSales(Pageable pageable, 
+			@RequestParam(value = "minDate", defaultValue = "") String minDate, 
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+		return saleService.findSales(pageable, minDate, maxDate);
 	}
 }
--- backend/src/main/java/br/com/gabrieldeoliveira/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/br/com/gabrieldeoliveira/dsmeta/repositories/SaleRepository.java
@@ -1,8 +1,16 @@
 package br.com.gabrieldeoliveira.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.gabrieldeoliveira.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+	
+	@Query("SELECT sale FROM Sale sale WHERE sale.date BETWEEN :min AND :max ORDER BY sale.amount DESC")
+	Page<Sale> findSales(Pageable pageable, LocalDate min, LocalDate max);
 }
